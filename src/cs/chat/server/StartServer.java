c920735cfe84ae86f31be04cf34254a1eacb27ea
csinensis12 <csinensis12@gmail.com>
2016-02-05 20:40:43 +0100

    changed extend -> implements, abstract classes ->interfaces

---
+++ src/cs/chat/server/StartServer.java
@@ -0,0 +1,14 @@
+package cs.chat.server;
+
+/**
+ * Created by szymo on 05/02/2016.
+ */
+public class StartServer {
+
+    public static void main(String[] args){
+        Input input = new ConsoleInput();
+        Output output = new ConsoleOutput();
+
+
+    }
+}
